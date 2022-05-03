d256bbd834c9ea626dd2a3e27333d45eefdb85d5
Ariana Milasincic <ariana@motorway.co.uk>
2022-05-03 15:24:30 +0100

    try: tagName and releaseName

--- dist/index.js
+++ dist/index.js
@@ -8763,14 +8763,14 @@ const core = __nccwpck_require__(8481);
 const github = __nccwpck_require__(4633);
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const tagName = core.getInput('tag_name');
+  const releaseName= core.getInput('release_name')
+  console.log(`Tag name: ${tagName}`);
+  console.log(`Release name: ${releaseName}`)
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+//   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
