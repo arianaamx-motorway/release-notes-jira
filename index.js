a140d483837036d88bc27fea62af45594da89a9d
Ariana Milasincic <ariana@motorway.co.uk>
2022-05-03 23:12:02 +0100

    print out event payload

--- index.js
+++ index.js
@@ -9,23 +9,14 @@ try {
 
   const octokit = github.getOctokit(githubToken)
 
-  let bodyFileContent = null;
-  try {
-     bodyFileContent = fs.readFileSync(bodyPath, { encoding: 'utf8' });
-  } catch (error) {
-      core.setFailed(error.message);
-  }
-  
   console.log(bodyFileContent)
   console.log(`Tag name: ${tagName}`);
   console.log(`Release name: ${releaseName}`)
   console.log(`Github token:  ${githubToken}`)
 
-
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   console.log(`The event payload: ${payload}`);
+  //   Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
